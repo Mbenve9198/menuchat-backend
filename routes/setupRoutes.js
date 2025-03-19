d0d87672c1b3fcac032f3e64f7213dc1ae0a3f1c
Marco <marco@midachat.com>
2025-03-19 17:50:25 +0100

    review

--- routes/setupRoutes.js
+++ routes/setupRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // Rotte pubbliche
 router.post('/', setupController.setupRestaurant);
 router.post('/generate-welcome-message', setupController.generateWelcomeMessage);
+router.post('/generate-review-templates', setupController.generateReviewTemplates);
 
 // Rotte protette (richiedono autenticazione)
 router.get('/:id', protect, setupController.getRestaurant);
