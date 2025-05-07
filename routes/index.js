6a79b6fa6097727652dfe1c816cc9d79ebe8b1a5
Marco <marco@midachat.com>
2025-05-07 18:59:00 +0200

    route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./authRoutes');
 const setupRoutes = require('./setupRoutes');
 const twilioRoutes = require('./twilioRoutes');
+const templateRoutes = require('./templateRoutes');
 
 const router = express.Router();
 
@@ -13,5 +14,8 @@ router.use('/setup', setupRoutes);
 
 // Rotte di Twilio - importante: per il webhook usiamo direttamente /twilio/webhook
 router.use('/twilio', twilioRoutes);
+
+// Rotte dei template
+router.use('/templates', templateRoutes);
 
 module.exports = router; 
